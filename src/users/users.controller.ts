4b9955183e7b4dcb6fd9d9cdb348aab803043f1a
jasonlimantoro <jason@ewburgers.com>
2021-04-13 00:47:15 +0800

    wip: add /users and /users/:id routes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { User } from './user.entity';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Get()
+  async findAll(): Promise<User[]> {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  async find(@Param('id') id: string): Promise<User> {
+    return this.usersService.findOne(id);
+  }
+}
---
+++ test/user.e2e-spec.ts
@@ -0,0 +1,47 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './../src/users/user.entity';
+import { UsersModule } from './../src/users/users.module';
+
+describe('UserController (e2e)', () => {
+  let app: INestApplication;
+
+  const mockUserRepository = {
+    find: jest.fn().mockResolvedValue([]),
+    findOne: jest.fn().mockResolvedValue({}),
+  };
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [UsersModule],
+    })
+      .overrideProvider(getRepositoryToken(User))
+      .useValue(mockUserRepository)
+      .compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  describe('/users', () => {
+    it('/ (GET)', async () => {
+      await request(app.getHttpServer())
+        .get('/users')
+        .expect(200, await mockUserRepository.find());
+      expect(mockUserRepository.find).toBeCalled();
+    });
+
+    it(':id (GET)', async () => {
+      await request(app.getHttpServer())
+        .get('/users/1')
+        .expect(200, await mockUserRepository.findOne());
+      expect(mockUserRepository.findOne).toBeCalledWith('1');
+    });
+  });
+});
